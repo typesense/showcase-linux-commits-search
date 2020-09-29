9f81bd95e51bae6e3b6d684f148e69d5a7725364
Jason Bosco <mail@jasonbos.co>
2020-09-28 18:37:35 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+
+const search = instantsearch({
+  indexName: 'linux_commit_history',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.start();
