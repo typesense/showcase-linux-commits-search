6b6c170326cb21dc87d36f623a2a5671adecfbf8
jiangbo212 <peiyaoliukuan@gmail.com>
2023-01-29 16:27:37 +0800

    init change

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ window.$ = jQuery; // workaround for https://github.com/parcel-bundler/parcel/is
 
 import 'popper.js';
 import 'bootstrap';
-
 import instantsearch from 'instantsearch.js/es';
 import {
   searchBox,
   infiniteHits,
   configure,
   stats,
-  analytics,
   refinementList,
   menu,
   sortBy,
@@ -19,116 +17,20 @@ import {
   rangeInput,
   toggleRefinement,
 } from 'instantsearch.js/es/widgets';
-import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
-import { SearchClient as TypesenseSearchClient } from 'typesense'; // To get the total number of docs
-import images from '../images/*.*';
-import STOP_WORDS from './utils/stop_words.json';
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
+import 'regenerator-runtime/runtime'
 
-let TYPESENSE_SERVER_CONFIG = {
-  apiKey: process.env.TYPESENSE_SEARCH_ONLY_API_KEY, // Be sure to use an API key that only allows searches, in production
-  nodes: [
-    {
-      host: process.env.TYPESENSE_HOST,
-      port: process.env.TYPESENSE_PORT,
-      protocol: process.env.TYPESENSE_PROTOCOL,
-    },
-  ],
-  numRetries: 8,
-  useServerSideSearchCache: true,
-};
-
-// [2, 3].forEach(i => {
-//   if (process.env[`TYPESENSE_HOST_${i}`]) {
-//     TYPESENSE_SERVER_CONFIG.nodes.push({
-//       host: process.env[`TYPESENSE_HOST_${i}`],
-//       port: process.env.TYPESENSE_PORT,
-//       protocol: process.env.TYPESENSE_PROTOCOL,
-//     });
-//   }
-// });
-
-// Unfortunately, dynamic process.env keys don't work with parcel.js
-// So need to enumerate each key one by one
-
-if (process.env[`TYPESENSE_HOST_2`]) {
-  TYPESENSE_SERVER_CONFIG.nodes.push({
-    host: process.env[`TYPESENSE_HOST_2`],
-    port: process.env.TYPESENSE_PORT,
-    protocol: process.env.TYPESENSE_PROTOCOL,
-  });
-}
-
-if (process.env[`TYPESENSE_HOST_3`]) {
-  TYPESENSE_SERVER_CONFIG.nodes.push({
-    host: process.env[`TYPESENSE_HOST_3`],
-    port: process.env.TYPESENSE_PORT,
-    protocol: process.env.TYPESENSE_PROTOCOL,
-  });
-}
-
-if (process.env[`TYPESENSE_HOST_NEAREST`]) {
-  TYPESENSE_SERVER_CONFIG['nearestNode'] = {
-    host: process.env[`TYPESENSE_HOST_NEAREST`],
-    port: process.env.TYPESENSE_PORT,
-    protocol: process.env.TYPESENSE_PROTOCOL,
-  };
-}
-
-const INDEX_NAME = process.env.TYPESENSE_COLLECTION_NAME;
-
-async function getIndexSize() {
-  let typesenseSearchClient = new TypesenseSearchClient(
-    TYPESENSE_SERVER_CONFIG
-  );
-  let results = await typesenseSearchClient
-    .collections(INDEX_NAME)
-    .documents()
-    .search({ q: '*' });
-
-  return results['found'];
-}
-
-let indexSize;
-
-(async () => {
-  indexSize = await getIndexSize();
-})();
-
-function queryWithoutStopWords(query) {
-  const words = query.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').split(' ');
-  return words
-    .map((word) => {
-      if (STOP_WORDS.includes(word.toLowerCase())) {
-        return null;
-      } else {
-        return word;
-      }
-    })
-    .filter((w) => w)
-    .join(' ')
-    .trim();
-}
-
-const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
-  server: TYPESENSE_SERVER_CONFIG,
-  // The following parameters are directly passed to Typesense's search API endpoint.
-  //  So you can pass any parameters supported by the search endpoint below.
-  //  queryBy is required.
-  additionalSearchParameters: {
-    queryBy: 'subject,body',
-    queryByWeights: '1,1',
-    dropTokensThreshold: 2,
-    typoTokensThreshold: 2,
-    numTypos: 1,
-  },
-});
-const searchClient = typesenseInstantsearchAdapter.searchClient;
+const HOST = process.env.MEILISEARCH_HOST;
+const API_KEY = process.env.MEILISEARCH_API_KEY;
+const INDEX_NAME = process.env.MEILISEARCH_INDEX_NAME;
 
 const search = instantsearch({
-  searchClient,
   indexName: INDEX_NAME,
-  routing: true,
-});
+  searchClient: instantMeiliSearch(
+    HOST,
+    API_KEY
+  ),
+})
 
 search.addWidgets([
   searchBox({
@@ -142,43 +44,30 @@ search.addWidgets([
       loadingIcon: 'stroke-primary',
     },
     queryHook(query, search) {
-      const modifiedQuery = queryWithoutStopWords(query);
-      search(modifiedQuery);
+      search(query);
     },
   }),
-
-  analytics({
-    pushFunction(formattedParameters, state, results) {
-      window.ga(
-        'set',
-        'page',
-        (window.location.pathname + window.location.search).toLowerCase()
-      );
-      window.ga('send', 'pageView');
-    },
-  }),
-
-  stats({
-    container: '#stats',
-    templates: {
-      text: ({ nbHits, hasNoResults, hasOneResult, processingTimeMS }) => {
-        let statsText = '';
-        if (hasNoResults) {
-          statsText = 'no commits';
-        } else if (hasOneResult) {
-          statsText = '1 commit';
-        } else {
-          statsText = `${nbHits.toLocaleString()} commits`;
-        }
-        return `Found ${statsText} ${
-          indexSize ? ` from ${indexSize.toLocaleString()}` : ''
-        } in ${processingTimeMS}ms.`;
-      },
-    },
-    cssClasses: {
-      text: 'text-muted',
-    },
-  }),
+  // stats({
+  //   container: '#stats',
+  //   templates: {
+  //     text: ({ nbHits, hasNoResults, hasOneResult, processingTimeMS }) => {
+  //       let statsText = '';
+  //       if (hasNoResults) {
+  //         statsText = 'no commits';
+  //       } else if (hasOneResult) {
+  //         statsText = '1 commit';
+  //       } else {
+  //         statsText = `${nbHits.toLocaleString()} commits`;
+  //       }
+  //       return `Found ${statsText} ${
+  //         indexSize ? ` from ${indexSize.toLocaleString()}` : ''
+  //       } in ${processingTimeMS}ms.`;
+  //     },
+  //   },
+  //   cssClasses: {
+  //     text: 'text-muted',
+  //   },
+  // }),
   infiniteHits({
     container: '#hits',
     cssClasses: {
