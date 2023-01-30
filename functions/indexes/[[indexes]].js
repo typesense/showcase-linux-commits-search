2355e25500b986633569111418c64c54d0aec469
jiangbo212 <peiyaoliukuan@gmail.com>
2023-01-30 12:44:12 +0800

    update function

--- functions/indexes/[[indexes]].js
+++ functions/indexes/[[indexes]].js
@@ -1,6 +1,6 @@
-export function onRequest(context) {  
+export async function onRequest(context) {  
     let request = context.request;
     const url = new URL(request.url)
     console.log("pathname", url.pathname);
-    return context.env.SERVICE.fetch(context.request);
+    return context.env.SERVICE.fetch(request);
 }
