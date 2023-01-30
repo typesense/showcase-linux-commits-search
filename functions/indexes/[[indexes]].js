5f2a809c1d65f7d13284a5f5330f1f3783166fbe
jiangbo212 <peiyaoliukuan@gmail.com>
2023-01-30 10:06:20 +0800

    add rediects

--- functions/indexes/[[indexes]].js
+++
@@ -1,7 +0,0 @@
-export function onRequest(context) {  
-    // return new Response("Hello, world!")
-    let request = context.request;
-    console.log(request.url);
-    console.log(request.pathname);
-    return context.env.ASSETS.fetch(context.request.url);
-}
