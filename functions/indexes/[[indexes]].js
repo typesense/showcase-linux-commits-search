3e9f84451c77452510b360625258981540501f0a
jiangbo212 <peiyaoliukuan@gmail.com>
2023-01-30 11:05:17 +0800

    update functions

--- functions/indexes/[[indexes]].js
+++ functions/indexes/[[indexes]].js
@@ -1,11 +1,7 @@
 export function onRequest(context) {  
     let request = context.request;
-    console.log("url", request.url);
-    let newUrl = "https://linux-commit-search-backend.jiangbo.space/indexes" + request.url.split('/indexes')[1];
-    console.log("newUrl", newUrl);
     const url = new URL(request.url)
-    console.log("pathname", url.pathname);
-    const result = context.env.ASSETS.fetch(newUrl);
-    console.log("result", result);
-    return result;
+    let newUrl = "https://linux-search-log.peiyao212.workers.dev" + url.pathname;
+    console.log("newUrl", newUrl);
+    return context.env.SERVICE.fetch(newUrl);
 }
