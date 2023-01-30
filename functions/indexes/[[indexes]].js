cea7442273190709df4ea3490f34f2f8baed29f6
jiangbo212 <peiyaoliukuan@gmail.com>
2023-01-30 10:41:28 +0800

    update function

--- functions/indexes/[[indexes]].js
+++ functions/indexes/[[indexes]].js
@@ -1,7 +1,11 @@
 export function onRequest(context) {  
     let request = context.request;
-    console.log("url:", request.url);
+    console.log("url", request.url);
     let newUrl = "https://linux-commit-search-backend.jiangbo.space/indexes" + request.url.split('/indexes')[1];
-    console.log("newUrl:", newUrl);
-    return context.env.ASSETS.fetch(newUrl);
+    console.log("newUrl", newUrl);
+    const url = new URL(request.url)
+    console.log("pathname", url.pathname);
+    const result = context.env.ASSETS.fetch(newUrl);
+    console.log("result", result);
+    return result;
 }
