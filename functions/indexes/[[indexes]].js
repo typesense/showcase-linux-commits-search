3f21f00dc6d986c8f9bb3fa0ee4947b538e8b6f9
jiangbo212 <peiyaoliukuan@gmail.com>
2023-01-30 11:14:09 +0800

    update functions

--- functions/indexes/[[indexes]].js
+++ functions/indexes/[[indexes]].js
@@ -1,7 +1,6 @@
 export function onRequest(context) {  
     let request = context.request;
     const url = new URL(request.url)
-    let newUrl = "https://linux-search-log.peiyao212.workers.dev" + url.pathname;
-    console.log("newUrl", newUrl);
-    return context.env.SERVICE.fetch(newUrl);
+    console.log("pathname", url.pathname);
+    return context.env.SERVICE.fetch(context.request);
 }
